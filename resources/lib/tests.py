71ccb54665d35d6d8c98685ad4f8657cc7479e27
heppen <32433090+heppen7@users.noreply.github.com>
2023-01-14 16:03:53 +0100

    some tests

---
+++ resources/lib/tests.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.insert(0, '')
+
+from resources.lib.plex.connections import Connections
+import cProfile
+
+conn = Connections()
+
+def profiler(func):
+    def wrapper(iterations):
+        with cProfile.Profile() as pr:
+            func(iterations)
+            
+            print(pr.print_stats())
+    return wrapper
+
+@profiler
+def library(id):
+    videos = conn.get_library(id)
+    for video in videos:
+        info = conn.meta_info(video)
+        art = conn.meta_art(video)
+        if video.get('key'):
+            print(f'ListItem(title={info["title"]})')
+        else:
+            print(f'ListItem(title={info["title"]})')   
+            
+library(1)
